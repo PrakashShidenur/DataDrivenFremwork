251353a017aa301902229df5b41ebb8e80da9a4a
Prakash <prakash.shidenur@gmail.com>
2020-06-14 11:51:49 +0530

    my second commit

---
+++ src/test/java/com/w2a/base/TestBase.java
@@ -0,0 +1,17 @@
+package com.w2a.base;
+
+public class TestBase {
+	
+	//initialization done here
+	/* Webdriver
+	 * properties
+	 * extent report
+	 * Logs
+	 * Mail
+	 * DB
+	 * Excel
+	 * 
+	 * 
+	 */
+
+}
